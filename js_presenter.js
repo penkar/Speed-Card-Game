a83c42afd10b55d231dda247a1ee05518fbfbe9a
Bradley Anderson <bcaytonanderson@gmail.com>
2014-09-25 13:55:01 -0500

    Started on js_presenter for player control.

--- js_presenter.js
+++ js_presenter.js
@@ -1,1 +1,31 @@
+var playObject = {
+    hand: null,
+    cardIndex: null,
+    playCard: null
+  };
 
+var resetPlayObject = function() {
+  playObject = {
+    hand: null,
+    cardIndex: null,
+    playCard: null
+  }
+}
+
+$('.start').on('click', function(){
+  $(document).trigger('newGame');
+
+  $('.red').not('.lowerDeck').on('click', function() {
+    playObject.hand = lowerHand;
+    var card = $(this).attr('class');
+    playObject.cardIndex = card.slice(-1);
+    console.log(playObject);
+  });
+
+  $('.playLeft, .playRight').on('click', function() {
+    playObject.playCard = $(this).attr('class').split(' ')[1];
+    console.log(playObject);
+    $(document).trigger('playAttempt', playObject);
+    resetPlayObject();
+  })
+});
