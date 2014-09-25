e6d2ec160f735da063838c1c1c9272b7d452086c
penkar <jspenkar@gmail.com>
2014-09-25 15:22:03 -0500

    Comp moves

--- computer_opponent.js
+++ computer_opponent.js
@@ -2,24 +2,22 @@
 // Create for loop that computer opponent will run through in order to play game. Each card will be played agains the left and right play card. Afterwards a draw card will trigger. 
 var compMove = function(active){
 	var active = true;
-	while(active){
-console.log('while loop')
-		active = false
-	var count = upperHand.length;
+		while(active){
+			active = false
+		var count = upperHand.length;
 
-		for(var i = 0; i < upperHand.length; i++){
-			setTimeout(function(){attemptMove(upperHand,i,playLeft)},2000);
-			setTimeout(function(){attemptMove(upperHand,i,playRight)},2000);
-			console.log(i)
-		} 
-		
-		if (upperHand.length !== count){
-console.log('bye')
-			active = true
-			do{drawCard(upperHand); count = upperHand.length;}
-				while(upperHand.count<5 && upperDeck.length==0);
+			for(var i = 0; i < upperHand.length; i++){
+				attemptMove(upperHand,i,playLeft);
+				attemptMove(upperHand,i,playRight);
+				console.log(i)
+			} 
+			
+			if (upperHand.length !== count){
+				active = true
+				do{drawCard(upperHand); count = upperHand.length;}
+					while(upperHand.count<5 && upperDeck.length==0);
+			}
 		}
-	}
 	setWin();
 }
 
