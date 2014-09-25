79882c04a692719d130a178fc8cb2dad35dda4a6
Bradley Anderson <bcaytonanderson@gmail.com>
2014-09-25 12:40:44 -0500

    Commented out code.

--- speed-logic.js
+++ speed-logic.js
@@ -4,6 +4,7 @@ var mainDeck, upperCounter, lowerCounter;
 var lowerDeck, upperDeck, drawLeft, drawRight, playLeft, playRight, lowerHand, upperHand, stats;
 
 //IIFE that makes sure there is a fresh deck when the page loads. This also sets all other play variables to null.
+//Card values being at Ace being 1, and continue to to D on a base-14 scale. A = 10, B = Jack, C = Queen, D = King.
 var resetDeck = (function () {
   mainDeck = [
   '1 C', '2 C', '3 C', '4 C', '5 C', '6 C', '7 C', '8 C', '9 C', 'A C', 'B C', 'C C', 'D C',
@@ -34,7 +35,8 @@ var shuffleDeck = function() {
   return mainDeck;
 };
 
-//First, it calls the shuffleDeck function to make sure that the cards are not dealt out in sequential order. It then deals out cards by removing them from the mainDeck and placing them into the play variables.
+//First, it calls the shuffleDeck function to make sure that the cards are not dealt out in sequential order.
+//It then deals out cards by removing them from the mainDeck and placing them into the play variables.
 var dealCards = function() {
   shuffleDeck();
   lowerDeck = mainDeck.splice(0, 15);
@@ -49,6 +51,13 @@ var dealCards = function() {
   lowerCounter = lowerDeck.length + lowerHand.length;
 };
 
+//Takes 3 arguments: the variable name of the hand being playing from (upperHand/lowerHand),
+//the index of the card being attempted in that hand, and the card in play being utilized (playLeft/playRight).
+//The method parses an integer from the string representing the card, using a base 14 radix. The if clause in the
+//conditional makes sure Aces are able to be played on either Kings or Twos; the else if clause makes sure Kings can
+//be played on Aces or Queens; and the else clause establishes that all other cards can be played on values either one
+//value higher or one value lower than itself. The makePlay method is called when the move is legal, and that method
+//takes the same arguments given to attemptMove.
 var attemptMove = function(hand, cardIndex, playCard) {
   attemptCard = parseInt(hand[cardIndex], 14);
   compareCard = parseInt(playCard[0], 14);
@@ -94,11 +103,17 @@ var attemptMove = function(hand, cardIndex, playCard) {
   }
 };
 
+//This function will always be called from the attemptMove function, and is given those same arguments. This function
+//removes the value from the playCard (center card being played on) and replaces it with the value of the player's card.
+//The card is then removed from the player's hand.
 var makePlay = function(hand, cardIndex, playCard) {
   playCard.splice(0, 1, hand[cardIndex]);
   hand.splice(cardIndex, 1);
 };
 
+//This function checks that the player's hand has fewer than five cards in it. If so, it then checks which hand is attempting
+//the draw, and makes sure that there is at least one card in the corresponding deck. If all conditions check out, a card
+//is spliced from the first index of that player's deck array and pushed into player's hand.
 var drawCard = function(hand) {
  if (hand.length < 5) {
     if (hand == lowerHand && lowerDeck.length > 0) {
@@ -127,3 +142,7 @@ var stats = function() {
     console.log(playRight);
     console.log(drawRight);
 };
+
+$(document).on('newGame', function() {
+  dealCards;
+})
