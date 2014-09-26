fdffc8c1236134285304444dc0e362d90dc85212
penkar <jspenkar@gmail.com>
2014-09-26 10:24:12 -0500

    Displays working properly.

--- computer_opponent.js
+++ computer_opponent.js
@@ -29,6 +29,7 @@ $(document).on('newGame', function (e, obj){
 
 // Create for loop that computer opponent will run through in order to play game. Each card will be played agains the left and right play card. Afterwards a draw card will trigger.
 var compMove = function(){
+	if(gameOver){return true};
 	(function(){
 		var count = upperHand.length;
 			for(var i = 0; i < count; i++){
@@ -47,11 +48,4 @@ var compMove = function(){
 	})();
 	setTimeout(drawInd = true,8000);
 	setWin();
-}
-// Paramaters to trigger a win for the computer.
-var setWin= function(){
-	if (upperHand.length === 0 && upperDeck.length === 0){
-		console.log('The Computer has beaten you.');
-	return true;
-	}
 }
